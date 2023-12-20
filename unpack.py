74bb7b2dda3561028f89acbf801bc0af70c57cc1
Maxion38 <147092829+Maxion38@users.noreply.github.com>
2023-12-20 12:32:21 +0100

    Added time

--- unpack.py
+++ unpack.py
@@ -1,6 +1,9 @@
 # Take all the files out of the forlders
 import shutil
 import os
+import time
+
+start_time = time.time()
 
 files_path = "C:\\Users\\mbong\\OneDrive\\Bureau\\python_photo_sort\\"
 
@@ -13,10 +16,12 @@ for dir in dir_list:
         destination_path = files_path + file
         shutil.move(current_file_path, destination_path)
     print(dir + " unpacked")
-print("All directories emptied")
+print("Directories emptied")
 
 for file_dir in dir_list:
     if os.path.isdir(files_path + "\\" + file_dir):
         os.rmdir(files_path + file_dir)
 
-print("All directories removed")
+print("Directories removed")
+end_time = time.time()
+print(str(round(end_time - start_time, 2)) + "s")
