5fd8c26312ff13b04e38126a85ee20e7533cfe5e
Maxion38 <147092829+Maxion38@users.noreply.github.com>
2023-12-20 12:33:16 +0100

    Main now run the gui

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-
-
+from gui import FolderSelectorApp
+from kivy.config import Config
+import os
 
 if __name__ == '__main__':
-    print('Running main')
+    Config.set('graphics', 'width', '550')
+    Config.set('graphics', 'height', '200')
+
+    f1 = FolderSelectorApp()
+    f1.run()
+    f1 = FolderSelectorApp()
+    print(os.path.normpath(f1.selected_folder))
