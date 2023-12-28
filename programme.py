d1dd13e8a35d55bd44add10a292430a259a18972
Maxion38 <147092829+Maxion38@users.noreply.github.com>
2023-12-28 14:50:08 +0100

    Adapting old project to an oo class: creating class OrderFiles and making all the specifications

--- programme.py
+++ programme.py
@@ -1,136 +1,80 @@
-from exiftool import ExifToolHelper
-import os
-import shutil
-from datetime import datetime
-import math
-import threading
-import time
+class OrderFiles:
+    """Class that extract metadata and can order files by date
+
+    Author : Bongartz Maxime
+    Date : December 2023
+    This class analyse metadata and order files using metadata
+    """
+
+    def __init__(self, dir_path, date_format):
+        """Keep the paths needed and get date metadata.
+
+        PRE : dir_path should be a valid dir path
+              date_format must be usable by strftime function
+        POST :  self.__dir_path is the dir selected to be sorted
+                self.__files_list is the list of files names in self.__dir_path
+                self.__treads_number is the optimal number of threads that will be used
+                self.files_meta is an empty dict
+                self.__date_format is the date format
+                self.__files_path_list is the list of files paths + names in self.__dir_path
+                self.__splited_files_list is the __files_path_list splited by the number of threads
+
+        """
+
+        pass
 
 
-def run_program(path, date_format):
-    start_time = time.time()
+    def __append_files_by_date(self, file_list):
+        """ Use exifTool to extract photos metadata (even from raw photos).
 
-    NUMBER_OF_TREADS = math.floor(os.cpu_count() / 1.14)  # optimal threads number (theory)
+        PRE : file_list must be a list of complete files paths ex: ['C:\\path\\IMG_0172.CR3', 'C:\\path\\IMG_0173.CR3']
+        POST : self.__files_meta is a dict() where keys are a date and values are the files with this date name
+        """
 
-    files_path = path + "\\"
-    files_list = os.listdir(files_path)
-    files_meta = {}
-
-
-    ''' Filter files list to 'dir' or 'file' '''
-    def filter_list(input_list, filter):
-        if filter == 'dir':
-            dir_list = []
-            for file in input_list:
-                if os.path.isdir(files_path + file):
-                    dir_list.append(file)
-
-            return dir_list
-
-        if filter == 'file':
-            file_list = []
-            for file in input_list:
-                if os.path.isfile(files_path + file):
-                    file_list.append(file)
-
-            return file_list
+        pass
 
 
-    ''' Split list into n lists '''
-    def split_list(input_list, chunck_number):
-        chunck_list = []
-        step = math.floor(len(input_list)/chunck_number)
-        start = 0
-        stop = 0
-        for i in range(chunck_number):
-            temp_list = []
-            stop += step
-            for pos in range(start, stop):
-                temp_list.append(input_list[pos])
+    def __use_threads(self, function, arg):
+        """ Uses threads with wanted functions.
 
-            chunck_list.append(temp_list)
-            start += step
+        PRE :   function must be a valid function
+                arg must be the arg of the function
+                arg should be a list with same length as the self.__treads_number
+        POST :  use self.__treads_number number of cpu cores to process wanted function faster
+        """
 
-        # Rest
-        rest = stop
-        i = 0
-        for pos in range(rest, len(input_list)):
-            chunck_list[i].append(input_list[pos])
-            i += 1
-        return chunck_list
-
-    ''' Use exifTool to extract photos metadata (even raw photos) '''
-    def get_metadata(file_list):
-        print("Analysing")
-        with ExifToolHelper() as et:
-            metadata_elem = et.get_tags(file_list, tags=["DateTimeOriginal"])
-
-            for file in metadata_elem:
-                day = datetime.strptime(file["EXIF:DateTimeOriginal"], "%Y:%m:%d %H:%M:%S")
-                day = day.strftime("%y-%m-%d")
-
-                if day not in files_meta:
-                    files_meta[day] = []
-
-                files_meta[day].append(file)
-
-        print("OK")
+        pass
 
 
-    ''' Os modifications '''
-    def os_files_sort(metadata_list):
-        for i, file in enumerate(metadata_list):
-            source_file_path = file["SourceFile"]
-            file_date_taken = file["EXIF:DateTimeOriginal"]
-            # Format date name
-            file_date_taken = datetime.strptime(file_date_taken, "%Y:%m:%d %H:%M:%S")
-            file_date_taken = file_date_taken.strftime(date_format)
+    def __split_list(self, input_list, chunck_number):
+        """ Split list into n lists.
 
-            # Move file in dir
-            destination_path = files_path + file_date_taken
-            source_file_path = os.path.normpath(source_file_path)
-            shutil.move(source_file_path, destination_path)
+        PRE : input_list must be a list
+              input_list must be > chunck_number
+        POST : returns chunck_list a list of chunck_number lists
+        """
+
+        pass
 
 
-    ''' Uses threads with wanted functions '''
-    def use_threads(function, func_arg):
-        # Open threads
-        threads = []
-        for i in range(NUMBER_OF_TREADS):
-            thread = threading.Thread(target=function, args=(func_arg[i],))
-            threads.append(thread)
-            thread.start()
+    def __filter_list(self, input_list, by):
+        """ Filter files list to 'dir' or 'file'.
 
-        # Wait threads
-        for thread in threads:
-            thread.join()
+        PRE : input_list must be a list of files names
+              by must only be 'dir' or 'file'
+        POST : returns file_list a list of file names filtered by the file or dir depending on the by param
+        """
+
+        pass
 
 
-    # Create files path list
-    files_path_list = filter_list(files_list, 'file')
-    for i, file in enumerate(files_path_list):
-        files_path_list[i] = files_path + file
+    def __os_files_sort(self, metadata_list):
+        """ Os modifications.
 
-    # Prepare multi threading
-    if NUMBER_OF_TREADS < len(files_path_list):
-        splited_files_list = split_list(files_path_list, NUMBER_OF_TREADS)
+        PRE : metadata_list is a dict, keys are strings / values is a list of dicts with filenames
+        POST : order the dir chosen at the beginning by creating dirs, their names are dates, then move the files in
+        correct dirs
 
-    # Get metadata
-    if NUMBER_OF_TREADS < len(files_path_list):
-        use_threads(get_metadata, splited_files_list)
-    else:
-        get_metadata(files_path_list)
+        """
 
-    for date in files_meta:
-        # Format date name
-        formated_date = datetime.strptime(date, "%y-%m-%d")
-        formated_date = formated_date.strftime(date_format)
-
-        # Create dir
-        os.mkdir(files_path + formated_date)
-
-        # Move photos
-        os_files_sort(files_meta[date])
-
-    end_time = time.time()
-    print(str(round(end_time - start_time, 2)) + "s")
+        pass
